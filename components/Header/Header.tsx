1f950ae7420f4b12a79e88e9f29c0fb3db162590
rkyerevan <rkyerevan@gmail.com>
2023-11-03 15:52:11 -0700

    Testing type check

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -3,9 +3,9 @@ import LogoWhite from "public/logo-white.svg";
 
 import styles from "./Header.module.scss"
 
-const Header  = () => {
+const Header  = (src:any) => {
     return <header className={styles.header}>
-        <Image src={LogoWhite} width={236}/>
+        <Image src={LogoWhite} width={236} alt="#"/>
     </header>
 }
 
